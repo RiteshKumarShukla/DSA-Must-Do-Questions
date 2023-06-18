8936fd3e660b367670c2ee395d57d2a27da35401
Ritesh Kumar Shukla <riteshshuklagem@gmail.com>
2023-06-18 08:04:39 +0530

    Find the longest common prefix among a set of strings.

---
+++ Strings/Q13.js
@@ -0,0 +1,26 @@
+// Find the longest common prefix among a set of strings.
+
+function longestCommonPrefix(strs) {
+    if (strs.length === 0) {
+        return "";
+    }
+
+    let prefix = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, prefix.length - 1);
+
+            if (prefix === "") {
+                return "";
+            }
+        }
+    }
+
+    return prefix;
+}
+
+// Example usage
+const strings = ["flower", "flow", "flight"];
+const commonPrefix = longestCommonPrefix(strings);
+console.log(commonPrefix); // Output: "fl"
