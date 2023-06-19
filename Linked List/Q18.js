747889a678e68cf030bf9f18cebc9ae66b233faa
Ritesh Kumar Shukla <riteshshuklagem@gmail.com>
2023-06-19 11:10:08 +0530

    Check if a linked list is a palindrome.

---
+++ Linked List/Q18.js
@@ -0,0 +1,46 @@
+// Check if a linked list is a palindrome.
+
+function isPalindrome(head) {
+    if (!head || !head.next) {
+        return true; // An empty list or a list with one node is considered a palindrome
+    }
+
+    // Step 1: Find the middle of the linked list
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    // Step 2: Reverse the second half of the linked list
+    let secondHalfHead = reverseList(slow);
+
+    // Step 3: Compare the reversed second half with the first half
+    let firstHalf = head;
+    let secondHalf = secondHalfHead;
+    while (firstHalf && secondHalf) {
+        if (firstHalf.val !== secondHalf.val) {
+            return false; // Elements don't match, not a palindrome
+        }
+        firstHalf = firstHalf.next;
+        secondHalf = secondHalf.next;
+    }
+
+    return true; // All elements match, it's a palindrome
+}
+
+// Helper function to reverse a linked list
+function reverseList(head) {
+    let prev = null;
+    let current = head;
+
+    while (current) {
+        let nextNode = current.next;
+        current.next = prev;
+        prev = current;
+        current = nextNode;
+    }
+
+    return prev;
+}
