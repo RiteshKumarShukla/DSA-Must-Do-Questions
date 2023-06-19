5f6bc6b409dfa6256859866cc4c5a6c2b894611c
Ritesh Kumar Shukla <riteshshuklagem@gmail.com>
2023-06-19 10:53:49 +0530

    Implement a singly linked list and perform basic operations like insertion and deletion.

--- Linked List/Q11.js
+++ Linked List/Q11.js
@@ -1,58 +1,77 @@
-// Implement a doubly linked list.
+//Implement a singly linked list and perform basic operations like insertion and deletion.
 
 function Node(data) {
     this.data = data;
-    this.prev = null;
     this.next = null;
 }
 
-function DoublyLinkedList() {
+function LinkedList() {
     this.head = null;
-    this.tail = null;
     this.size = 0;
 }
 
-DoublyLinkedList.prototype.append = function (data) {
+LinkedList.prototype.insertAtEnd = function (data) {
     const newNode = new Node(data);
 
     if (!this.head) {
         this.head = newNode;
-        this.tail = newNode;
     } else {
-        newNode.prev = this.tail;
-        this.tail.next = newNode;
-        this.tail = newNode;
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = newNode;
     }
 
     this.size++;
 };
 
-DoublyLinkedList.prototype.prepend = function (data) {
+LinkedList.prototype.insertAtBeginning = function (data) {
     const newNode = new Node(data);
 
     if (!this.head) {
         this.head = newNode;
-        this.tail = newNode;
     } else {
         newNode.next = this.head;
-        this.head.prev = newNode;
         this.head = newNode;
     }
 
     this.size++;
 };
 
-DoublyLinkedList.prototype.insertAt = function (data, position) {
+LinkedList.prototype.insertAtPosition = function (data, position) {
     if (position < 0 || position > this.size) {
         throw new Error('Invalid position');
     }
 
-    const newNode = new Node(data);
+    if (position === 0) {
+        this.insertAtBeginning(data);
+    } else if (position === this.size) {
+        this.insertAtEnd(data);
+    } else {
+        const newNode = new Node(data);
+        let current = this.head;
+        let count = 0;
+
+        while (count < position - 1) {
+            current = current.next;
+            count++;
+        }
+
+        newNode.next = current.next;
+        current.next = newNode;
+
+        this.size++;
+    }
+};
+
+LinkedList.prototype.removeFromPosition = function (position) {
+    if (position < 0 || position >= this.size) {
+        throw new Error('Invalid position');
+    }
 
     if (position === 0) {
-        this.prepend(data);
-    } else if (position === this.size) {
-        this.append(data);
+        this.head = this.head.next;
     } else {
         let current = this.head;
         let count = 0;
@@ -62,53 +81,13 @@ DoublyLinkedList.prototype.insertAt = function (data, position) {
             count++;
         }
 
-        newNode.prev = current;
-        newNode.next = current.next;
-        current.next.prev = newNode;
-        current.next = newNode;
-
-        this.size++;
-    }
-};
-
-DoublyLinkedList.prototype.removeAt = function (position) {
-    if (position < 0 || position >= this.size) {
-        throw new Error('Invalid position');
-    }
-
-    let current = this.head;
-
-    if (position === 0) {
-        this.head = current.next;
-        if (this.head) {
-            this.head.prev = null;
-        } else {
-            this.tail = null;
-        }
-    } else if (position === this.size - 1) {
-        current = this.tail;
-        this.tail = current.prev;
-        this.tail.next = null;
-    } else {
-        let count = 0;
-
-        while (count < position) {
-            current = current.next;
-            count++;
-        }
-
-        current.prev.next = current.next;
-        current.next.prev = current.prev;
+        current.next = current.next.next;
     }
 
     this.size--;
 };
 
-DoublyLinkedList.prototype.getSize = function () {
-    return this.size;
-};
-
-DoublyLinkedList.prototype.display = function () {
+LinkedList.prototype.display = function () {
     let current = this.head;
     let result = '';
 
@@ -119,3 +98,22 @@ DoublyLinkedList.prototype.display = function () {
 
     console.log(result.trim());
 };
+
+LinkedList.prototype.getSize = function () {
+    return this.size;
+};
+
+
+//Example Usage :
+const linkedList = new LinkedList();
+
+linkedList.insertAtEnd(10);
+linkedList.insertAtEnd(20);
+linkedList.insertAtBeginning(5);
+linkedList.insertAtPosition(15, 2);
+linkedList.display(); // Output: 5 10 15 20
+
+linkedList.removeFromPosition(2);
+linkedList.display(); // Output: 5 10 20
+
+console.log(linkedList.getSize()); // Output: 3
